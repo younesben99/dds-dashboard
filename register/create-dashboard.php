9d31acdb7be976ac40568f9a95e4f0afcc421456
Younes Benkheil <younesbenkheil@gmail.com>
2021-01-20 02:51:16 +0100

    update

--- register/create-dashboard.php
+++ register/create-dashboard.php
@@ -3,10 +3,12 @@
 
 if(!file_exists(ABSPATH . "dashboard/index.php")){
     
-    if(!is_dir($ABSPATH . "dashboard")) {
+    if(!is_dir(ABSPATH . "dashboard")) {
         mkdir(ABSPATH . "dashboard");
     }
-
+    if(!is_dir(ABSPATH . "dashboard/archief")) {
+        mkdir(ABSPATH . "dashboard/archief");
+    }
     $myfile = fopen(ABSPATH . "login.php", "w");
     $content = "<?php 
     include(__DIR__ .'/wp-load.php');
@@ -22,7 +24,7 @@ if(!file_exists(ABSPATH . "dashboard/index.php")){
     fclose($myfile);
 
 
-    $myfile = fopen(ABSPATH . "dashboard/archief.php", "w");
+    $myfile = fopen(ABSPATH . "dashboard/archief/index.php", "w");
     $content = "<?php 
     include(__DIR__ .'/../wp-load.php');
     include( WP_PLUGIN_DIR. '/dds-dashboard/templates/dashboard-archive.php'); ?>";
@@ -30,27 +32,6 @@ if(!file_exists(ABSPATH . "dashboard/index.php")){
     fclose($myfile);
 
 
-    $myfile = fopen(ABSPATH . ".htaccess", "w");
-    $content = "RewriteEngine on 
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteCond %{REQUEST_FILENAME}\.php -f
-RewriteRule ^(.*)$ $1.php [NC,L]
-# BEGIN WordPress
-# dynamically generated, and should only be modified via WordPress filters.
-# Any changes to the directives between these markers will be overwritten.
-<IfModule mod_rewrite.c>
-RewriteEngine On
-RewriteRule .* - [E=HTTP_AUTHORIZATION:%{HTTP:Authorization}]
-RewriteBase /
-RewriteRule ^index\.php$ - [L]
-RewriteCond %{REQUEST_FILENAME} !-f
-RewriteCond %{REQUEST_FILENAME} !-d
-RewriteRule . /index.php [L]
-</IfModule>
-
-# END WordPress";
-    fwrite($myfile, $content);
-    fclose($myfile);
     
 
 }
